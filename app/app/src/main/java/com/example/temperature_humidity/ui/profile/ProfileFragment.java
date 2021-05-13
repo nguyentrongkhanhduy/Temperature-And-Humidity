2abefb659ec8d2a784f1db24035d293f0277db4a
Dung <qd24052000@gmail.com>
2021-05-13 20:40:11 +0700

    add edit progile

--- app/app/src/main/java/com/example/temperature_humidity/ui/profile/ProfileFragment.java
+++ app/app/src/main/java/com/example/temperature_humidity/ui/profile/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.temperature_humidity.ui.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +13,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.temperature_humidity.EditProfileActivity;
 import com.example.temperature_humidity.R;
 import com.example.temperature_humidity.databinding.FragmentProfileBinding;
 
@@ -28,10 +31,20 @@ public class ProfileFragment extends Fragment {
         ((AppCompatActivity) getActivity()).getSupportActionBar().show();
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        binding.btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getActivity(), EditProfileActivity.class);
+                in.putExtra("some","some data");
+                startActivity(in);
+
+            }
+        });
 
 
         return root;
     }
+
 
     @Override
     public void onDestroyView() {
