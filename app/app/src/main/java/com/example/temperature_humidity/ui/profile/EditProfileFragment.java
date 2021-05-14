29a2c7fe87670174184d646a2df4abaa758b3a06
Dung <qd24052000@gmail.com>
2021-05-14 15:12:19 +0700

    change editprofileactivity to editprofilefragment

--- app/app/src/main/java/com/example/temperature_humidity/ui/profile/EditProfileFragment.java
+++ app/app/src/main/java/com/example/temperature_humidity/ui/profile/EditProfileFragment.java
@@ -1,38 +1,36 @@
- package com.example.temperature_humidity.ui.profile;
+package com.example.temperature_humidity.ui.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.temperature_humidity.ui.profile.EditProfileFragment;
 import com.example.temperature_humidity.R;
-import com.example.temperature_humidity.databinding.FragmentProfileBinding;
+import com.example.temperature_humidity.databinding.FragmentEditprofileBinding;
 
-public class ProfileFragment extends Fragment {
+
+public class EditProfileFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
-    private FragmentProfileBinding binding;
+    private FragmentEditprofileBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 new ViewModelProvider(this).get(DashboardViewModel.class);
         ((AppCompatActivity) getActivity()).getSupportActionBar().show();
-        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        binding = FragmentEditprofileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        binding.btnEdit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(root).navigate(R.id.profile_to_editprofile);
-            }
-        });
 
         return root;
     }
